a300c60d7307d45006dbe60dc3f2b2709147efd3
HassanKachawa <99229716+HassanKachawa@users.noreply.github.com>
2023-06-15 11:21:45 +0530

    Add files via upload

---
+++ Assign1.py
@@ -0,0 +1,7 @@
+
+Name=input("Enter your Name:")
+M1=int(input("Enter your M1 Marks:"))
+M2=int(input("Enter your M2 Marks:"))
+M3=int(input("Enter your M3 Marks:"))
+Avg=(M1+M2+M3)/3
+print("Name:",Name,"Marks:\n M1",M1,"\n","M2:",M2,"\n","M3:",M3,"\n","Average:",Avg)
